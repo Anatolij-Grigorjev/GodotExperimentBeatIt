8b706b13e37df711c1d41262e268f92e18b80703
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2017-09-28 11:25:37 +0300

    added sprite rotation for direction

--- game/player/player.gd
+++ game/player/player.gd
@@ -7,17 +7,22 @@ const MOVESPEED_Y = 50
 
 #quick access to animator
 onready var anim = get_node("anim")
+onready var sprites_attack = get_node("sprites_attack")
+onready var sprites_move = get_node("sprites_move")
 
 var curr_anim = ""
+var current_sprite
 
 func _fixed_process(delta):
 	
 	var next_anim = CONST.PLAYER_ANIM_IDLE
+	current_sprite = sprites_move
 	#gather inputs
 	var move_left = Input.is_action_pressed("move_left")
 	var move_right = Input.is_action_pressed("move_right")
 	var move_up = Input.is_action_pressed("move_up")
 	var move_down = Input.is_action_pressed("move_down")
+	
 	
 	var pos = get_pos()
 	var move_vector = Vector2()
@@ -36,7 +41,13 @@ func _fixed_process(delta):
 	#some movement involved
 	if (move_vector.length_squared() != 0):
 		next_anim = CONST.PLAYER_ANIM_WALK
+		#flip sprite if direction change
+		if (move_vector.x < 0 and move_left):
+			current_sprite.set_scale(Vector2(-1.0, 1.0))
+		elif (move_vector.x > 0 and move_right):
+			current_sprite.set_scale(Vector2(1.0, 1.0))
 	
+
 	if (curr_anim != next_anim):
 		curr_anim = next_anim
 		anim.play(curr_anim)

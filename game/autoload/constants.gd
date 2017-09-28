b14fa5cf91ddd60c60f2e55384c6fe2f64f4da88
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2017-09-28 21:48:07 +0300

    approaching a double tap running mechanic

--- game/autoload/constants.gd
+++ game/autoload/constants.gd
@@ -6,12 +6,20 @@
 #to use
 extends Node
 
+#player animations names
 const PLAYER_ANIM_IDLE = "idle"
 const PLAYER_ANIM_WALK = "walk"
+const PLAYER_ANIM_RUN_START = "run_start"
+const PLAYER_ANIM_RUN_CONTINUE = "run_continue"
 
+#player input actions, defined in project settings
 const INPUT_ACTION_MOVE_LEFT = "move_left"
 const INPUT_ACTION_MOVE_RIGHT = "move_right"
 const INPUT_ACTION_MOVE_UP = "move_up"
 const INPUT_ACTION_MOVE_DOWN = "move_down"
+
+#how many milliseconds of input pause are allowed
+#to be considered a double-tap
+const DOUBLE_TAP_INTERVAL = 250
 
 
--- game/player/player.gd
+++ game/player/player.gd
@@ -2,8 +2,10 @@ extends Area2D
 #quick access to constnats
 onready var CONST = get_node("/root/const")
 
-const MOVESPEED_X = 100
-const MOVESPEED_Y = 50
+const MOVESPEED_X_WALK = 100
+const MOVESPEED_Y_WALK = 50
+var WALK_SPEED = Vector2(MOVESPEED_X_WALK, MOVESPEED_Y_WALK)
+
 
 #quick access to animator
 onready var anim = get_node("anim")
@@ -12,11 +14,17 @@ onready var sprites_move = get_node("sprites_move")
 
 var curr_anim = ""
 var current_sprite
+var move_vector = Vector2()
+var running = false
+var last_movement_tap
 
 func _fixed_process(delta):
 	
+	#initial frame logic
 	var next_anim = CONST.PLAYER_ANIM_IDLE
 	current_sprite = sprites_move
+	var pos = get_pos()
+	move_vector = Vector2()
 	#gather inputs
 	var move_left = Input.is_action_pressed(CONST.INPUT_ACTION_MOVE_LEFT)
 	var move_right = Input.is_action_pressed(CONST.INPUT_ACTION_MOVE_RIGHT)
@@ -24,16 +32,17 @@ func _fixed_process(delta):
 	var move_down = Input.is_action_pressed(CONST.INPUT_ACTION_MOVE_DOWN)
 	
 	
-	var pos = get_pos()
-	var move_vector = Vector2()
 	if (move_left):
-		move_vector.x = -MOVESPEED_X
+		move_vector.x = -1
 	if (move_right):
-		move_vector.x = MOVESPEED_X
+		move_vector.x = 1
 	if (move_up):
-		move_vector.y = -MOVESPEED_Y
+		move_vector.y = -1
 	if (move_down):
-		move_vector.y = MOVESPEED_Y
+		move_vector.y = 1
+	
+	#pick movement speed
+	move_vector *= WALK_SPEED
 	
 	#integrate new position
 	set_pos(pos + (move_vector * delta))
@@ -51,7 +60,15 @@ func _fixed_process(delta):
 	if (curr_anim != next_anim):
 		curr_anim = next_anim
 		anim.play(curr_anim)
-
+	
+	#set up for next frame
+	
+	#if there was move input in this frame, lets record when and to where
+	if (move_vector.length_squared() > 0):
+		last_movement_tap = {
+			"time": OS.get_unix_time(),
+			"direction": move_vector.normalized()
+		}
 
 func _ready():
 	set_fixed_process(true)

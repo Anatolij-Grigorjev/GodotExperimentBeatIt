01757e2a2694f9f62481f647ff53b2ac76a87dfe
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2017-10-12 20:45:11 +0300

    trying to switch to Q based input model for attacks

--- game/player/player_attack.gd
+++ game/player/player_attack.gd
@@ -7,9 +7,7 @@ onready var ATTACK_ANIMATIONS = [
 	CONST.PLAYER_ANIM_ATTACK_1,
 	CONST.PLAYER_ANIM_ATTACK_2
 ]
-#howe much time at end of attack animation next input is awaited
-const ANIMATION_INPUT_TAIL = 0.2
-
+const INPUT_Q_SIZE = 15
 #access to main character node
 onready var parent = get_node("../")
 #access to movement, to know what attack to switch to
@@ -25,70 +23,58 @@ var hitting = false
 var attacking
 var curr_anim = ""
 var attack_state
-var input_waits = {}
-var input_wait = 0.0
-var next_anim 
+var inputs = []
+var inputs_idx
+var inputs_insert_idx
 
 func _ready():
 	attacking = false
 	attack_state = null
-	for animation in ATTACK_ANIMATIONS:
-		input_waits[animation] = anim.get_animation(animation).get_length() - ANIMATION_INPUT_TAIL
-		
 	anim.play(CONST.PLAYER_ANIM_ATTACK_IDLE)
+	inputs = []
+	inputs.resize(INPUT_Q_SIZE)
+	inputs_idx = 0
+	inputs_insert_idx = 0
 	set_process(true)
 	
 func _process(delta):
-	#start attack
-	next_anim = null
-	if (!attacking and Input.is_action_pressed(CONST.INPUT_ACTION_ATTACK)):
-		attacking = true
-		#cant move at start of attack
-		locked = true
-		attack_state = ATTACK1
-		next_anim = CONST.PLAYER_ANIM_ATTACK_1
-		input_wait = input_waits[next_anim]
-	elif (attacking):
-		input_wait -= delta
-		if (input_wait < 0.0):
-			locked = false
-			if (Input.is_action_pressed(CONST.INPUT_ACTION_ATTACK)):
-				if (attack_state < ATTACK2 
-					or (hitting and attack_state < ATTACK4)):
-					locked = true
-					attack_state += 1
-					next_anim = "attack_" + str(attack_state + 1)
-					input_wait = input_waits[next_anim]
-					#also add to input the current animation length
-					input_wait += (anim.get_current_animation_length() - anim.get_current_animation_pos())
-				else:
-					reset_attack_state()
-			#combo finished
+	
+	var next_action = inputs[inputs_idx]
+	if (next_action != null):
+		inputs_idx = (inputs_idx + 1) % INPUT_Q_SIZE
+		#start of attack
+		if (!attacking):
+			attacking = true
+			#cant move at start of attack
+			locked = true
+			attack_state = ATTACK1
+			anim.play(CONST.PLAYER_ANIM_ATTACK_1)
+		else:
+			#already attacking, extend combo due to action
+			if (attack_state < ATTACK2):
+				locked = true
+				attack_state += 1
+				anim.queue("attack_" + str(attack_state + 1))
 			else:
-				#finish current attack naimation first
-				if (anim.is_playing()):
-					input_wait = anim.get_current_animation_length() - anim.get_current_animation_pos()
-				else: 	
+				if (!anim.is_playing()):
 					reset_attack_state()
-				
-	if (next_anim == null and !attacking):
-		next_anim = CONST.PLAYER_ANIM_ATTACK_IDLE
-		anim.play(next_anim)
+					
+	elif (!attacking):
+		reset_attack_state()
+		anim.play(CONST.PLAYER_ANIM_ATTACK_IDLE)
 		parent.switch_mode(false)
 	
-	if (next_anim != null and next_anim != curr_anim):
-		#if this is a legitimate attack sequence, we wait for previous 
-		#animation to stop playing first
-		if (curr_anim.substr(0, 6) == "attack"):
-			anim.queue(next_anim)
-		else:
-			anim.play(next_anim)
-		curr_anim = next_anim
+	#gather inputs for next frame
+	if (Input.is_action_pressed(CONST.INPUT_ACTION_ATTACK)):
+		inputs.insert(CONST.INPUT_ACTION_ATTACK, inputs_insert_idx)
+		inputs_insert_idx = (inputs_insert_idx + 1) % INPUT_Q_SIZE
 
 
 func reset_attack_state():
 	locked = false
 	attacking = false
 	attack_state = null
-	next_anim = null
-	input_wait = 0.0
+	for idx in range(inputs.size()):
+		inputs[idx] = null
+	inputs_idx = 0
+	inputs_insert_idx = 0

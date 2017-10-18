16ea171f8195a00b7840550e8a448a6a54310222
Anatolij Grigorjev <anatolij@mediapark.lt>
2017-10-18 14:14:10 +0300

    initial thoughts on enemy states

--- game/characters/enemies/enemy.gd
+++ game/characters/enemies/enemy.gd
@@ -2,20 +2,34 @@ extends "../basic_movement.gd"
 
 const DECISION_INTERVAL = 2.5 #in seconds
 
-var getting_hit = false
+enum STATES {STANDING, MOVING, ATTACKING, HURTING}
+
+var current_state
 var current_decision_wait
 
+onready var anim = get_node("anim")
+
 func _ready():
+	current_state = STANDING
 	current_decision_wait = DECISION_INTERVAL
 	._ready()
 	
 func _process(delta):
 	
-	if (!getting_hit):
+	if (current_state != HURTING):
 		#make deiscions
 		current_decision_wait -= delta
 		if (current_decision_wait < 0):
-			#make decision
+			#make decision, due to upper if, 
+			#unlikely to be HURTING here
+			if (current_state == STANDING):
+				pass
+			elif (current_state == MOVING):
+				pass
+			elif(current_state == ATTACKING):
+				pass
+			else:
+				current_state = STANDING
 			current_decision_wait = DECISION_INTERVAL
 		
 	._process(delta)
